416404f7ea5c563a3b93d196ef0604c4435b0378
Chloé VIEILLE <37896835+ChloeViei@users.noreply.github.com>
2018-10-02 15:21:57 -0400

    interface client avec une classe python #12

--- src/Client/Interface.py
+++ src/Client/Interface.py
@@ -1,17 +1,47 @@
-import tkinter as tk
+from tkinter import *
+import os
+import bluetooth
+
+class Interface(Frame):
+
+    """Notre fenêtre principale
+    Tous les widgets sont stockés comme attributs de cette fenêtre."""
 
 
-root = tk.Tk()
+    def __init__(self, fenetre, **kwargs):
+        Frame.__init__(self, fenetre, width=768, height=576, **kwargs)
+        self.pack(fill=BOTH)
+        self.nb_clic = 0
+
+        # Création de nos widgets
+        self.message = Label(self, text="Vous n'etes connecte a aucune bouee.")
+        self.message.pack()
+
+        self.boutonConnexion = tk.Button(root, text="connexion", command = self.connexionServeur)
+        self.boutonConnexion.grid(row=0,column=0)
+
+        self.boutonTelechargement = tk.Button(root, text="Telechargement")
+        self.boutonTelechargement.grid(row=1,column=0)
+
+        self.boutonTeleversement = tk.Button(root, text="Televersement")
+        self.boutonTeleversement.grid(row=2,column=0)
 
 
+    def connexionServeur(self):
+        """Il y a eu un clic sur le bouton, on change la valeur du label message
+        Si on detecte une bouee on se connecte et ainsi on recoit les donnees"""
 
-boutonConnexion = tk.Button(root, text="connexion")
-boutonConnexion.grid(row=0,column=0)
+        nearby_devices = bluetooth.discover_devices(lookup_names = True, flush_cache = True, duration = 20)
 
-boutonTelechargement = tk.Button(root, text="Telechargement")
-boutonTelechargement.grid(row=1,column=0)
+        for addr, name in nearby_devices:
+            print (" %s - %s" % (addr, name))
+            self.message["text"] = "Bouée détectee : {} ".format(addr)
 
-boutonTeleversement = tk.Button(root, text="Televersement")
-boutonTeleversement.grid(row=2,column=0)
+        os.system("python bluetooth.py client")
 
-root.mainloop()
+
+fenetre = Tk()
+interface = Interface(fenetre)
+
+interface.mainloop()
+interface.destroy()
