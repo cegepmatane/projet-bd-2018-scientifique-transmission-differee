be70497b0afe1b1a2667b0da847a787945959c70
Chloé VIEILLE <37896835+ChloeViei@users.noreply.github.com>
2018-10-05 12:18:40 -0400

    Lien entre l'interface et le fonctionnement du client #12

--- src/Client/Client.py
+++ src/Client/Client.py
@@ -1,43 +1,54 @@
 import bluetooth
+import donneeDAO
+
+
+adresseServeur = ["",""]  # enregistrement des toutes les adresses MAC du recepteur bluetooth de toutes les bouees existante
 
 
 def connexionAuServeur():
-    # connexion bluetooth au serveur (Raspberry) en acceptant la connexion emise celui-ci
-    adresseServeur = ["",""]  # enregistrement des adresses MAC du recepteur bluetooth de toute les bouees
-    port = 1250
+    """ la connexion repose sur le fait de verifier si il existe une bouee (pour le moment on ne prend pas en
+    compte la possibiltie qu'il puisse en avoir plusieur) """
+
+    nearby_devices = bluetooth.discover_devices(lookup_names = True, flush_cache = True, duration = 20)
+
+    boueeProximite = ""
+    for addr, name in nearby_devices:
+        print (" %s - %s" % (addr, name))
+
+        # boucle verifiant que l'appareil detecte est bien une bouee
+        for i in adresseServeur:
+            if addr == i :
+                boueeProximite = addr
+
+    # retourne l'addresse de la bouee a laquel nous pouvons nous connecter
+    return boueeProximite
+
+
+
+def recuperationDonnee():
+    """ recuperationDonnee se connecte a la bouee et recupere les donnees envoyees par celle-ci """
+
+    port = 1   #verifie la connexion de l'appareil a un port specifique ainsi autant garder le meme
     backlog = 1  # nombre maximum d'appareil pouvant se connecter au serveur sur le port
     size = 1024
 
-    for serveur in adresseServeur:
-        ## RFCOMM type de socket pour le type de socket bluetooth
-        client_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-        client_sock.bind((serveur, port))
-        client_sock.listen(backlog)
+    # # recupere l'adresse de la bouee a proximite pour se connecter et recuperer les donnees
+    adresseServeur = connexionAuServeur()
 
-        return serveur
+    client_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)  ## RFCOMM type de socket pour le type de socket bluetooth
+    client_sock.bind((adresseServeur, port))
+    client_sock.listen(backlog)
 
-
-
-def recuperationDonnee(adresseServeur):
     ## accepte la connexion du serveur
     serveur_sock, adresse = client_sock.accept()
 
     ## recupere les donnees envoyee par le serveur
     donnee = serveur_sock.recv(size)
-    print ("donnees recuperées :", donnee)
+
+    # # permet de les enregistrer mais n'est pas operationnel pour l'instant
+    # donneeDAO.enregistementDonnee(json.loads(donnee))
+    print ("donnees recuperees :", donnee)
 
     ## ferme les sockets
     client_sock.close()
     serveur_sock.close()
-
-    # while True:
-    #     serveur_sock, serveurInfo = client_sock.accept()
-    #     try:
-    #         while True:
-    #             donnee = serveur_sock.recv(size)
-    #             if donnee:
-    #                 print(donnee)
-    #     except:
-    #         print("Fermeture du socket")
-    #         serveur_sock.close()
-    # client_sock.close()
--- src/Client/Interface.py
+++ src/Client/Interface.py
@@ -1,12 +1,14 @@
 import tkinter as tk
-import os
-import bluetooth
+import Client
+import connexionNodeJS
 
 class Interface(tk.Frame):
 
     """Notre fenetre principale
     Tous les widgets sont stockes comme attributs de cette fenetre."""
 
+    connexion = False
+    adresseServeur = ""
 
     def __init__(self, fenetre, **kwargs):
         tk.Frame.__init__(self, fenetre, width=768, height=576, **kwargs)
@@ -14,30 +16,40 @@ class Interface(tk.Frame):
         self.nb_clic = 0
 
         # Creation de nos widgets
-        self.message = tk.Label(self, text="Vous n'etes connecte a aucune bouee.")
-        self.message.pack()
-
         self.boutonConnexion = tk.Button(self, text="connexion", command = self.connexionServeur)
         self.boutonConnexion.pack()
 
-        self.boutonTelechargement = tk.Button(self, text="Telechargement")
+        self.boutonTelechargement = tk.Button(self, text="Telechargement", command = self.recuperationDonnee, state="disabled")
         self.boutonTelechargement.pack()
 
-        self.boutonTeleversement = tk.Button(self, text="Televersement")
+        self.boutonTeleversement = tk.Button(self, text="Televersement", command = self.envoieDonnee, state="disabled")
         self.boutonTeleversement.pack()
 
 
+
     def connexionServeur(self):
-        """Il y a eu un clic sur le bouton, on change la valeur du label message
-        Si on detecte une bouee on se connecte et ainsi on recoit les donnees"""
+        """Change la valeur du label message pour afficher les bouees a proimite
+        permet de ce connecter a la bouee souhaite"""
 
-        nearby_devices = bluetooth.discover_devices(lookup_names = True, flush_cache = True, duration = 20)
+        """Amelioration possible en permettant de selectionner la bouee souhaiter a proximite si il en existe plusieurs """
 
-        for addr, name in nearby_devices:
-            print (" %s - %s" % (addr, name))
-            self.message["text"] = "Bouee detectee : {} ".format(addr)
+        # permet d'acceder au bouton de telechargement ou le televersement
+        adresseServeur = Client.connexionAuServeur()
+        print (adresseServeur)
+        if adresseServeur != "" :  # si il y a une bouee a proximite
+            self.boutonTelechargement.config(state="normal")
 
-        os.system("python bluetooth.py client")
+
+    def recuperationDonnee(self):
+        """Lance la récupération des données"""
+        Client.recuperationDonnee()
+
+
+    def envoieDonnee(self):
+        """Envoie les donnees au serveur par internet pour le moment non operationnel, il faut d'abord verifier si
+        notre base donnee possede des donnees  et si on peut se connecter au serveur"""
+        connexionNodeJS.on_send()
+
 
 
 fenetre = tk.Tk()
