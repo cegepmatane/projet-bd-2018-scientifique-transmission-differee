979598ddfbb5b006d02120a26d3bc1c5c9d346d9
DouvryValentin <valentin.jabba@gmail.com>
2018-11-30 14:27:23 -0500

    Ajout internationalisation #13

--- src/Client/TestInterfaceListeAppareils.py
+++ src/Client/TestInterfaceListeAppareils.py
@@ -3,15 +3,20 @@ from bluetooth import *
 from subprocess import *
 import bluetooth
 import sys
+import locale
 from tkinter import messagebox
 from tkinter import Label
+
 global ListeAppareils
 global listeAdresse
 global fenetreListeAppareils
+global langage
+
+langage = locale.getdefaultlocale()[0].split('_')[0]
+print(langage)
 
 #---------------------------------------------------------------#
 fenetrePrincipale = Tk()
-fenetrePrincipale.title('Transmission differee')
 fenetrePrincipale.geometry("300x150")
 fenetrePrincipale.resizable(0,0)
 
@@ -29,7 +34,7 @@ def afficherListeAppareils():
     global fenetreListeAppareils
 
     fenetreListeAppareils = Tk()
-    fenetreListeAppareils.title("Liste appareils")
+    fenetreListeAppareils.title(changementLangue("List of devices"))
 
     frameListeAppareils = Frame(master=fenetreListeAppareils,bg='grey')
     frameListeAppareils.pack_propagate(0)
@@ -38,7 +43,7 @@ def afficherListeAppareils():
     for ligne in range(len(ListeAppareils)):
         for colonne in range(2):
             Label(frameListeAppareils, text='[%s]' % ', '.join(map(str, ListeAppareils[ligne])),borderwidth=1).grid(row=ligne, column=1,padx=5,pady=5)
-            Button(frameListeAppareils, text='Se connecter',command=lambda address=ligne: seConnecter(listeAdresse[address])).grid(row=ligne, column=2,padx=5,pady=5)
+            Button(frameListeAppareils, text=changementLangue("Connect"),command=lambda address=ligne: seConnecter(listeAdresse[address])).grid(row=ligne, column=2,padx=5,pady=5)
 
     fenetreListeAppareils.protocol("WM_DELETE_WINDOW", on_closing)
     fenetreListeAppareils.mainloop()
@@ -46,7 +51,7 @@ def afficherListeAppareils():
 #--------------------------------------------------------------------------#
 def on_closing():
     global listeAdresse
-    if messagebox.askokcancel("Quitter","Fermer la liste des appareils ?"):
+    if messagebox.askokcancel(changementLangue("Quit"),changementLangue("Close the list of devices?")):
         fenetreListeAppareils.destroy()
         boutonConnexion.config(state="disabled")
         listeAdresse.clear()
@@ -110,20 +115,39 @@ def seConnecter(addresse):
         print ('Reception : ', data)
     sock.close()"""
 #----------------------------------------------------------------------#
+#Changement langue 
+def changementLangue(s):
+    global langage
+
+    spanishStrings = {'Delayed transmission': 'Transmisión retrasada', 'List of devices': 'Lista de dispositivos', 'Connect': 'Conectar', 'Quit': 'Dejar', 'Close the list of devices?': 'Cerrar la lista de dispositivos?', 'Find devices': 'Encontrar dispositivos', 'Choose a device': 'Elija un dispositivo', 'Download data': 'Descargar datos', 'Upload data': 'Subir datos'}
+    frenchStrings = {'Delayed transmission': 'Transmission differee', 'List of devices': 'Liste des appareils', 'Connect': 'Se connecter', 'Quit': 'Quitter', 'Close the list of devices?': 'Fermer la liste des appareils ?', 'Find devices': 'Rechercher des appareils', 'Choose a device': 'Choisir un appareil', 'Download data': 'Telecharger les donnees', 'Upload data': 'Televerser les donnees'}
+    germanStrings = {'Delayed transmission': 'Verspätete Übertragung', 'List of devices': 'Liste der Geräte', 'Connect': 'Verbinden', 'Quit': 'Verlassen', 'Close the list of devices?': 'Schließen Sie die Liste der Geräte?', 'Find devices': 'Geräte suchen', 'Choose a device': 'Wähle ein Gerät', 'Download data': 'Daten herunterladen', 'Upload data': 'Daten hochladen'}
+
+    if langage == 'en':
+        return s
+    if langage == 'es':
+        return spanishStrings[s]
+    if langage == 'fr':
+        return frenchStrings[s]
+    if langage == 'de':
+        return germanStrings[s]
+#----------------------------------------------------------------------#
 
 
 #print('[%s]' % ', '.join(map(str, listeAdresse)))
 
-boutonRechercher = Button(master=framePrincipale,text="Rechercher des appareils", command = listerAppareils)
+fenetrePrincipale.title(changementLangue("Delayed transmission"))
+
+boutonRechercher = Button(master=framePrincipale,text=changementLangue("Find devices"), command = listerAppareils)
 boutonRechercher.pack(padx=5,pady=5)
 
-boutonConnexion = Button(master=framePrincipale,text="Choisir un appareil", command = afficherListeAppareils, state="disabled")
+boutonConnexion = Button(master=framePrincipale,text=changementLangue("Choose a device"), command = afficherListeAppareils, state="disabled")
 boutonConnexion.pack(padx=5,pady=5)
 
-boutonTelecharger = Button(master=framePrincipale,text="Telecharger les donnees", state="disabled")
+boutonTelecharger = Button(master=framePrincipale,text=changementLangue("Download data"), state="disabled")
 boutonTelecharger.pack(padx=5,pady=5)
 
-boutonTeleverser = Button(master=framePrincipale,text="Televerser les donnees", state="disabled")
+boutonTeleverser = Button(master=framePrincipale,text=changementLangue("Upload data"), state="disabled")
 boutonTeleverser.pack(padx=5,pady=5)
 
 fenetrePrincipale.mainloop()
