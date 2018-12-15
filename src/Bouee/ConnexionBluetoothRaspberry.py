a1aba514098d2cf32096688cb2b0c0bca50ef871
Douvry <valentin.jabba@gmail.com>
2018-12-14 20:32:11 -0500

    #5 script bluetooth pour le raspberry pour envoie des donnees en differe

--- src/Bouee/ConnexionBluetoothRaspberry.py
+++ src/Bouee/ConnexionBluetoothRaspberry.py
@@ -1,19 +1,29 @@
 from bluetooth import *
-
-HOST = ''
-PORT = 3
-socket=BluetoothSocket( RFCOMM )
-
-socket.bind((HOST, PORT))
-socket.listen(1)
-
-connexion, addresse = socket.accept()
-
-print ('Connecte a : ', addresse)
+import donneeDAO
 
 while True:
-    data = "coucou"
-    if not data: break
-    connexion.send(data)
+    HOST = ''
+    PORT = 3
+    socket=BluetoothSocket( RFCOMM )
 
-connexion.close()
+    socket.bind((HOST, PORT))
+    socket.listen(1)
+
+    connexion, addresse = socket.accept()
+    data = ""
+    data = donneeDAO.recupererValeurDifferee()
+    print ('Connecte a : ', addresse)
+    pretEnvoyer = None
+
+    if addresse is not None :
+        while pretEnvoyer is None:
+            pretEnvoyer = connexion.recv(1024)
+
+        if pretEnvoyer is not None:
+            connexion.send(data)
+            data = ""            
+            pretEnvoyer = None
+            
+        connexion.close()
+
+    
